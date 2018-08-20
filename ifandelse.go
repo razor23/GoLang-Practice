e74ad8ffa2af2865a93a14a115da3abacdfdc025
V1N <vinith.johnson@gmail.com>
2018-08-20 00:47:15 -0700

    ifelse with v,lim

---
+++ ifandelse.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+import "math"
+
+func power(x,n,lim float64) float64{
+  if v:=math.Pow(x,n);v<lim{
+    return v
+  }else {
+
+  fmt.Printf("%g is > %g",v,lim)
+}
+return lim
+}
+
+func main(){
+  fmt.Println(power(3,2,3))
+  fmt.Println(power(3,3,41))
+}
